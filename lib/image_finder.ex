a5c5319f3d439e03e58f2e4b4549be458be0fd5d
matias <example@example>
2019-10-29 21:40:02 -0300

    add todo comments

--- lib/image_finder.ex
+++ lib/image_finder.ex
@@ -2,10 +2,12 @@ defmodule ImageFinder do
   use Application
 
   def start(_type, _args) do
+    #TODO estos supervisores son estaticos, tendrian que ser dinamicos y ser supervisados por uno estatico
     ImageFinder.Supervisor.start_link
     Fetcher.Supervisor.start_link
     Saver.Supervisor.start_link
     Digester.Supervisor.start_link
+    #TODO poner un registry que tenga key value de dominios y pids, para ir mandando los fetch
   end
  
   def fetch(source_file, target_directory) do
